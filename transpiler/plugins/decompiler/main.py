4be69b460aa5b5c18988e7461c935f09aae6d015
771835 <150520586+771835@users.noreply.github.com>
2025-10-18 17:17:10 +0800

    修复了标准库一个拼接错误 添加了一个插件用于反编译和调试ir生成，通过DECOMPILER环境变量启用

---
+++ transpiler/plugins/decompiler/main.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+from .cora import CodeGenerator
+from transpiler.plugins.plugin_api_v1 import Plugin, registry_backend
+
+
+class Decompiler(Plugin):
+    def __init__(self):
+        super().__init__()
+
+    def load(self):
+        registry_backend(CodeGenerator)
+
+    def unload(self):
+        pass
+
+    def validate(self):
+        return True
