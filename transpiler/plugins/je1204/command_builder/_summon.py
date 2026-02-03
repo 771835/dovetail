a792606ac4ad843340504d91ddeaa2bc94cfcdd4
771835 <150520586+771835@users.noreply.github.com>
2026-02-03 15:59:36 +0800

    移除了严重过时的je1204

--- transpiler/plugins/je1204/command_builder/_summon.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-
-class SummonBuilder:
-    @staticmethod
-    def summon(entity: str, x: float | int, y: float | int, z: float | int, nbt: str):
-        return f"summon {entity} {x} {y} {z} {nbt}"
