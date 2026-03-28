2070e8a16312c1592b855030094d4a9652b2a742
771835 <150520586+771835@users.noreply.github.com>
2026-03-28 15:36:39 +0800

    更新了构建脚本

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
-python -m nuitka --standalone --lto=yes --deployment  --include-module=dovetail.utils.escape_processor --include-plugin-directory=dovetail/plugins/plugin_api --include-data-dir=lib=lib --include-data-dir=docs=docs --include-data-dir=proposals=proposals --include-data-dir=example=example --output-dir=build --output-filename=dovetail.exe  .\main.py
+python -m nuitka --standalone --lto=yes --deployment --include-module=dovetail.utils.escape_processor --include-plugin-directory=dovetail/plugins/plugin_api --include-data-dir=lib=lib --include-data-dir=docs=docs --include-data-dir=proposals=proposals --include-data-dir=example=example --output-dir=build --output-filename=dovetail.exe  .\main.py
 xcopy dovetail\plugins\ build\main.dist\plugins\ /s /y
 xcopy lark\ build\main.dist\lark\ /s /y
+xcopy NOTICE build\main.dist\ /y
+xcopy LICENSE build\main.dist\ /y
